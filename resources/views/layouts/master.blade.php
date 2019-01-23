4795d91d4027551f81fbcf3445b4988d8af0eaa8
Patric Pförtner <patricpfortner@Patrics-MacBook-Pro.local>
2019-01-23 18:25:07 +0200

    hi

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -31,15 +31,6 @@
     {{ $headerBlock or '' }}
 </head>
 <body class="slides zen noPreload simplifiedMobile animated">
-<div id="fb-root"></div>
-<script>(function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1191339764326043";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
 <svg xmlns="http://www.w3.org/2000/svg" style="display:none">
 
     <symbol id="close" viewBox="0 0 30 30">
