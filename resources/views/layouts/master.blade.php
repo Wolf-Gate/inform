f1b497f73d9194b25bb6d8aaa45e60784858b99e
Patric Pförtner <patricpfortner@Patrics-MacBook-Pro.local>
2019-01-23 15:40:27 +0200

    hi

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -348,6 +348,7 @@
 <script src="{{ asset('assets/js/slides.js') }}" type="text/javascript" name="plugins"></script>
 <script src="{{ asset('assets/js/plugins.js') }}" type="text/javascript" name="plugins"></script>
 <script src="{{ asset('assets/js/custom.js') }}" type="text/javascript" name="plugins"></script>
+<script async="async" src="https://static.mobilemonkey.com/js/202088929814490.js"></script>
 {{ $bottomBlock or '' }}
 </body>
 </html>
